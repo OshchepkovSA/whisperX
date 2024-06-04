170130be2c2de5c5837b8acb8a8c3d1d1a44e343
Sergey Oshchepkov <87944684+OshchepkovSA@users.noreply.github.com>
2024-06-04 17:30:10 +0400

    Update asr.py

--- whisperx/asr.py
+++ whisperx/asr.py
@@ -218,12 +218,13 @@ class FasterWhisperPipeline(Pipeline):
         batch_size = batch_size or self._batch_size
         total_segments = len(vad_segments)
 
-        vad_segments = []
-        for chunk in range(int(len_audio / (chunk_size * SAMPLE_RATE)) + 1):
-            start = chunk*chunk_size*SAMPLE_RATE
-            end = min(len_audio, (chunk+1)*(chunk_size * SAMPLE_RATE))
-            segments = [(start, end)]
-            vad_segments.append({"start": start, "end": end, "segments": segments})
+        # vad_segments = []
+        # for chunk in range(int(len_audio / (chunk_size * SAMPLE_RATE)) + 1):
+        #     start = chunk*chunk_size*SAMPLE_RATE
+        #     end = min(len_audio, (chunk+1)*(chunk_size * SAMPLE_RATE))
+        #     segments = [(start, end)]
+        #     vad_segments.append({"start": start, "end": end, "segments": segments})
+        print(type(vad_segments))
         
         for idx, out in enumerate(self.__call__(data(audio, vad_segments), batch_size=batch_size, num_workers=num_workers)):
             if print_progress:
